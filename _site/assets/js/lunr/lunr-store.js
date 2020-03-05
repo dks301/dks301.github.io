9190b67f88f673d2932eac8145b2672f4c4fcb68
hj <a301dks@naver.com>
2020-03-05 19:06:35 +0900

    _config의 timezon을 서울로 수정2

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Hello World",
+        "excerpt":"#Hello World!!  ","categories": ["Programming"],
+        "tags": ["Java","Web","Back-end"],
+        "url": "http://localhost:4000/programming/",
+        "teaser":null}]
