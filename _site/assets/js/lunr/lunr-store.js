d9e94c99942cc6a5782688ed7d1f8e95edc56aa5
hj <a301dks@naver.com>
2020-03-07 14:04:57 +0900

    share false로 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Hello World",
-        "excerpt":"#Hello World!!  ","categories": ["Programming"],
+        "excerpt":"#Hello World!!     ","categories": ["Programming"],
         "tags": ["Java","Web","Back-end"],
         "url": "http://localhost:4000/programming/",
         "teaser":null}]
