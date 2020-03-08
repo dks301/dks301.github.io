e2359697068224fa31447bafc90506274d19762a
hj <a301dks@naver.com>
2020-03-08 18:35:54 +0900

    폰트 사이즈를 large, xlarge기준 20px에서 18px로 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "마크 다운 문법 정리",
         "excerpt":"마크 다운 문법 정리  Github 블로그나 Repository의 README.md를 작성하기 위한 마크 다운 문법 정리입니다.  markdown 문법, html의 화면 순으로 작성했습니다.   1. 줄바꿈  마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다. 줄바꿈을 하려면 문장 끝에 스페이스를 2번   해야합니다.   마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다. 줄바꿈을 하려면 줄의 끝에 스페이스를 2번   해야합니다.   ","categories": ["Programming"],
-        "tags": ["Blog","Github pages","README"],
+        "tags": ["Blog","Github pages","README","Markdown"],
         "url": "http://localhost:4000/programming/",
         "teaser":null}]
