ee5e6c98e1673c5dda3b4f69e9f8a21aa1ed948d
hj <a301dks@naver.com>
2020-03-09 16:02:25 +0900

    코드 키친 헤더 이미지 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "마크 다운 문법 정리",
-        "excerpt":"마크 다운 문법 정리  Github 블로그나 Repository의 README.md를 작성하기 위한 마크 다운 문법 정리입니다.  markdown 문법, html의 화면 순으로 작성했습니다.   1. 줄바꿈  마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다. 줄바꿈을 하려면 문장 끝에 스페이스를 2번   해야합니다.   마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다. 줄바꿈을 하려면 줄의 끝에 스페이스를 2번   해야합니다.   ","categories": ["Programming"],
+        "excerpt":"마크 다운 문법 정리 Github 블로그나 Repository의 README.md를 작성하기 위한 마크 다운 문법 정리입니다. 제가 보려고 실제 markdown으로 작성한 글, html으로 보여지는 화면 순으로 작성했습니다. 공백을 직접 보시려면 드래그를 해보세요. 1. 줄바꿈 마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다. 줄바꿈을 하려면 라인 끝에 스페이스를 2번 넣어 주세요. 마크 다운은 줄바꿈(개행)이 자동으로 되지않습니다....","categories": ["Programming"],
         "tags": ["Blog","Github pages","README","Markdown"],
         "url": "http://localhost:4000/programming/",
         "teaser":null}]
